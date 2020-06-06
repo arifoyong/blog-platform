17914bb7cf283fbf8d07c002f39fbd2503fff98e
Arif <arifoyong@gmail.com>
2020-06-06 22:12:46 +0800

    Add css to nprogress

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -24,6 +24,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
           />
+          <link rel="stylesheet" href="/css/styles.css" />
         </Head>
         <body>
           <Main />
