3527ba7bf205ba2705dbd176df5ac5f52558fc9d
Arif <arifoyong@gmail.com>
2020-06-13 23:29:01 +0800

    Add categories & tags to blog page

--- frontend/components/crud/blogCreate.js
+++ frontend/components/crud/blogCreate.js
@@ -24,6 +24,8 @@ const BlogCreate = ({ router }) => {
     }
   };
 
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
   const [body, setBody] = useState(blogFromLocalStore());
   const [values, setValues] = useState({
     error: "",
@@ -45,7 +47,29 @@ const BlogCreate = ({ router }) => {
 
   useEffect(() => {
     setValues({ ...values, formData: new FormData() });
+    initCategories();
+    initTags();
   }, [router]);
+
+  const initCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  const initTags = () => {
+    getTags().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setTags(data);
+      }
+    });
+  };
 
   const publishBlog = (e) => {
     e.preventDefault();
@@ -67,6 +91,30 @@ const BlogCreate = ({ router }) => {
     if (typeof window !== "undefined") {
       localStorage.setItem("blog", JSON.stringify(e));
     }
+  };
+
+  const showCategories = () => {
+    return (
+      categories &&
+      categories.map((c, i) => (
+        <li key={i} className="list-unstyled">
+          <input type="checkbox" className="mr-2" />
+          <label className="form-check-label">{c.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  const showTags = () => {
+    return (
+      tags &&
+      tags.map((t, i) => (
+        <li key={i} className="list-unstyled">
+          <input type="checkbox" className="mr-2" />
+          <label className="form-check-label">{t.name}</label>
+        </li>
+      ))
+    );
   };
 
   const createBlogForm = () => {
@@ -101,10 +149,32 @@ const BlogCreate = ({ router }) => {
   };
 
   return (
-    <div>
-      {/* <h2>create blog component</h2> */}
-      {/* {JSON.stringify(router)} */}
-      {createBlogForm()}
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-8">
+          {createBlogForm()}
+          <p>categoris</p>
+          <br />
+          {JSON.stringify(categories)}
+          <br />
+          <p>tags</p>
+          {JSON.stringify(tags)}
+        </div>
+        <div className="col-md-4">
+          <div>
+            <h5>Categories</h5>
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showCategories()}
+            </ul>
+          </div>
+          <div>
+            <h5>Tags</h5>
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showTags()}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
